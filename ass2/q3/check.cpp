76c3bedb19d3b573769f6b2b9cd6266f17057b50
zaryab khan <115345789+zaryabk69180621@users.noreply.github.com>
2024-11-24 01:42:03 +0500

    Add files via upload

---
+++ ass2/q3/check.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+void reverse(char arr1[], int size){
+
+char *n= new char[size+1];
+for(int i=size-1;i>=0;i--){
+cout<<"a";
+n[size-1-i]=arr1[i];
+
+}
+n[size]='\0';
+cout<<endl<<"inside is "<<n;
+
+
+
+}
+int main(){
+
+
+char arr1[4]={'a','b','c','\0'};
+reverse(arr1,3);
+
+
+}
